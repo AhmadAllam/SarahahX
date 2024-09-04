525e657a1b0de7fe2a7fc27b64678adf4fb2b477
Ahmad Allam <86953062+AhmadAllam@users.noreply.github.com>
2024-09-04 14:24:07 +0300

    update user agent get method

--- src2/get_data.php
+++ src2/get_data.php
@@ -1,20 +1,23 @@
 <?php
 //main variables
-$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-$user_agent = $_SERVER['HTTP_USER_AGENT'];
+$ip = !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
+$user_agent = !empty($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'Unknown User Agent';
 mkdir($ip, 0777, true);
-$file_path = $ip. "/info.txt";
+$file_path = $ip . "/info.txt";
 $file = fopen($file_path, 'a');
 fwrite($file, " \r \n " );
+
 //get dname
 header('Content-Type: text/html');
-$dname = $_GET['dname'];
+$dname = isset($_GET['dname']) ? $_GET['dname'] : 'No dname provided';
 fwrite($file, $dname . "\n");
+
 //get ip with user agent
 fwrite($file, "IP Address: " . $ip . "\n\n");
 fwrite($file, "User Agent: " . $user_agent . "\n\n");
 fwrite($file, " ############################ " );
 fclose($file);
+
 //echo_me
 echo "Data written successfully to file.";
 ?>
